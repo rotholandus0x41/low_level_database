a18632259f38b948f3574039720ce18aebbac031
rotholandus0x41 <m_rotholandus@pm.me>
2026-09-21 14:32:46 +0200

    put_in_w_hash

--- src/kv.c
+++ src/kv.c
@@ -1,16 +1,94 @@
 #include <kv.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define TOMBSTONE 0x1
+
+size_t hash(char *val, int capacity)
+{
+    size_t hash = 0x13371337deadbeef;
+
+    while(*val)
+    {
+        hash ^= *val;
+        hash = hash << 8;
+        hash += *val;
+
+        val++;
+    }
+
+    return hash % capacity;
+}
+
+/*
+    fn kv_put
+    params:
+        - db: a pointer to the db
+        - key: a pointer to the key value
+        - value: a pointer to the value itself
+    returns: the index of the key, otherwise on
+    error, returns -1, on not found return -2
+*/
+
+int kv_put(kv_t *db, char *key, char *value)
+{
+    if (!db ||!key || !value) return -1;
+
+    size_t idx = hash(key, db->capacity);
+
+    for (int i = 0; i < db->capacity; i++)
+    {
+        size_t real_idx = (idx + i) % db->capacity;
+        kv_entry_t *entry = &db->entries[real_idx];
+
+        // the key is aldready set, updating
+        if (entry->key &&
+             entry->key != (void*)TOMBSTONE &&
+             !strcmp(entry->key, key))
+        {
+            char *newval = strdup(value);
+            if (!newval) return -1;
+            entry->value = newval;
+            return real_idx;
+        }
+
+        // land in a slot that is "empty"
+        // null or tombstone
+        if (!entry->key || entry->key == (void*)TOMBSTONE)
+        {
+            char *newval = strdup(value);
+            char *newkey = strdup(key);
+            if (!newval || !newkey)
+            {
+                free(newkey);
+                free(newval);
+                return -1;
+            }
+            entry->value = newval;
+            entry->key = newkey;
+            db->count++;
+            return real_idx;
+        }
+    }
+
+    // the db is occuped
+    return -1;
+    
+}
+
+// function for init a table for a database
 kv_t *kv_init(size_t capacity)
 {
     if (capacity == 0) return NULL;
-
+    // malloc pour resize la structure
     kv_t *table = malloc(sizeof(kv_t));
-
+    // error return null 
     if (table == NULL) return NULL;
 
     table->capacity = capacity;
     table->count = 0;
 
+    // calloc free la memoire!
     table->entries = calloc(sizeof(kv_entry_t), capacity);
     if (table->entries == NULL) return NULL;
 
--- src/main.c
+++ src/main.c
@@ -5,4 +5,15 @@ int main()
 {
     kv_t *table = kv_init(3);
     printf("%p", table);
+    printf("%lld\n", table->capacity);
+
+    kv_put(table, "hehe", "haha");
+
+    for (int i = 0; i < table->capacity; i++)
+    {
+        if (table->entries[i].key)
+        {
+            printf("%s: %s\n", table->entries[i].key, table->entries[i].value);
+        }
+    }
 }
